9bbb6163e24a76ccac6215eaaf848dd90a32688e
cheapjack <ross@cheapjack.org.uk>
2018-01-26 11:40:50 +0000

    update hyperloop images

--- python/templates/t_senhouseLeeds_work_template.py
+++ python/templates/t_senhouseLeeds_work_template.py
@@ -7,17 +7,17 @@ def render(engine, story, box, node, card, sack):
     yield from render1()
 #
     def render2(*a, **d):
-        if not(node.isHidden(engine, 'altars')):
-            yield """Make a spiritual visit
-to the altars: """
-            yield str(story.lookupNode('altars').getGoalBox(story).label)
-            yield """
-"""
         if not(node.isHidden(engine, 'seaview')):
             yield """Look out for attack
 from the Northern
 Sea!: """
             yield str(story.lookupNode('seaview').getGoalBox(story).label)
+            yield """
+"""
+        if not(node.isHidden(engine, 'altars')):
+            yield """Make a spiritual visit
+to the altars: """
+            yield str(story.lookupNode('altars').getGoalBox(story).label)
             yield """
 """
     yield from render2()
