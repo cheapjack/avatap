8eca93c2d35fb14cc3bed6bac98f1a874d325869
cheapjack <ross@cheapjack.org.uk>
2018-03-21 22:09:32 +0000

    prep for skeletonAves

--- python/stories/__init__.py
+++ python/stories/__init__.py
@@ -1,19 +1,9 @@
 #123456789012345678901234
-#introText="""
-#Welcome to Milecastles!
-#Look for numbered boxes!
-#Place tag on the symbol
-#& keep in place to read
-#then lift & replace to
-#progress the story...
-#"""
-#123456789012345678901234
 introText="""
-Welcome to ArcadeOfBabel!
-Look for numbered boxes!
-Place tag on the symbol
-& keep in place to read
-then lift & replace to
+Welcome to CLASS AVES!
+Look for books like this!
+PLACE tag on tag symbol
+& KEEP in place to READ,
+then LIFT & REPLACE to
 progress the story...
 """
-
